c93b1f6135420d640db7be5fa25bcc54a7a243fa
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-06 12:19:29 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -22,7 +22,7 @@ class PetsController < ApplicationController
 
   get '/pets/:id' do
     @pet = Pet.find(params[:id])
-    @owner = Pet.owner
+    @owner = @pet.owner
     erb :'/pets/show'
   end
 
