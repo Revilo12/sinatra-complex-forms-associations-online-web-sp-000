5c8fbf7bd123215aaceb2d6301352d5c065de6ff
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-06 12:02:11 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,7 +12,7 @@ class PetsController < ApplicationController
 
   post '/pets' do
     @pet = Pet.create(params[:pet])
-    if !(params[:pet][:name] == "")
+    if params[:owner][:name].empty?
       @pet.owner << Owner.create(name: params[:owner][:name])
     end
     redirect "/pets/#{@pet.id}"
