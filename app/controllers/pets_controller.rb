27fcc5711cfc03ff5a30f45a2937949bafa82d08
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-06 12:42:47 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -33,8 +33,8 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do
-    @pet = Pet.find(params[:id])
     binding.pry
+    @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
       @pet.owner= Owner.create(name: params["owner"]["name"])
